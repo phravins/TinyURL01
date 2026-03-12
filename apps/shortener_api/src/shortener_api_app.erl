c64ddb37a3afb307988ebb10f9852f9db1337672
phravins <164343676+phravins@users.noreply.github.com>
2026-03-12 15:20:02 +0530

    Initialize HTTP API application with Cowboy web server

--- apps/shortener_api/src/shortener_api_app.erl
+++ apps/shortener_api/src/shortener_api_app.erl
@@ -6,20 +6,32 @@
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
         {'_', [
-            {"/api/shorten", shortener_handler_shorten, []},
-            {"/api/stats/:code", shortener_handler_stats, []},
-            {"/:code", shortener_handler_redirect, []}
+            %% --- Core Endpoints ---
+            {"/api/shorten",             shortener_handler_shorten,  []},
+            {"/api/stats/:code",         shortener_handler_stats,    []},
+            %% --- Advanced Endpoints ---
+            {"/api/qr/:code",            shortener_handler_qr,       []},
+            {"/api/preview/:code",       shortener_handler_preview,  []},
+            {"/api/unlock/:code",        shortener_handler_unlock,   []},
+            %% --- Admin Endpoints ---
+            {"/api/admin/stats",         shortener_handler_admin,    []},
+            {"/api/admin/urls",          shortener_handler_admin,    []},
+            {"/api/admin/urls/:code",    shortener_handler_admin,    []},
+            {"/api/admin/analytics/:code", shortener_handler_admin,  []},
+            %% --- Redirect (catch-all, must be last) ---
+            {"/:code",                   shortener_handler_redirect, []}
         ]}
     ]),
-    
+
     Port = application:get_env(shortener_api, port, 8080),
-    
+
     {ok, _} = cowboy:start_clear(http,
         [{port, Port}],
         #{env => #{dispatch => Dispatch}}
     ),
-    
+
     shortener_api_sup:start_link().
 
 stop(_State) ->
+    cowboy:stop_listener(http),
     ok.
--- apps/shortener_api/src/shortener_api_sup.erl
+++ apps/shortener_api/src/shortener_api_sup.erl
@@ -8,8 +8,17 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 5,
+                 period => 10},
+    ChildSpecs = [
+        #{
+            id      => shortener_rate_limiter,
+            start   => {shortener_rate_limiter, start_link, []},
+            restart => permanent,
+            shutdown => 5000,
+            type    => worker,
+            modules => [shortener_rate_limiter]
+        }
+    ],
     {ok, {SupFlags, ChildSpecs}}.
